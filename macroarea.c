01805662bee68fa6b87c77ee8f4e9a21daab40eb
Alex-S07 <alexsajued@gmail.com>
2025-04-09 07:53:51 +0530

    Update macroarea.c

--- macroarea.c
+++ macroarea.c
@@ -1,21 +1,28 @@
 #include<stdio.h>
-#define Pi 3.14159
-#define circ(r) Pi*r*r
-#define sqr(x) x*x
-#define tri(b,h) {0.5*b*h}  
-void main(){
-	int r ;
-	printf("enter the radius of the circle :");
-	scanf("%d",&r);
-	printf("Area of the circle is :%f\n",circ(r));
-	
-	int x;
-	printf("enter the side of the square:");
-	scanf("%d",&x);
-	printf("Are of the square is :%d\n",sqr(x));
-	
-	int b,h;
-	printf("enter the base and height of the triangle:");
-	scanf("%d%d",&b,&h);
-	printf("Area of the triangle is :%d\n",tri(b,h));
+#define PI 3.14159
+#define CIRCLE(r) (PI * r * r)
+#define SQUARE(a) (a * a)
+#define TRIANGLE(b, h) (0.5 * b * h)
+#define CYLINDER(d, h) (2 * PI * d * (d + h))
+
+int main() {
+    int r, a, b, h, d, h1;
+
+    printf("Enter the radius of the circle: ");
+    scanf("%d", &r);
+    printf("The area of the circle = %f\n", CIRCLE(r));
+
+    printf("Enter the side of the square: ");
+    scanf("%d", &a);
+    printf("The area of the square = %d\n", SQUARE(a));
+
+    printf("Enter the base and height of the triangle: ");
+    scanf("%d %d", &b, &h);
+    printf("The area of the triangle = %f\n", TRIANGLE(b, h));
+
+    printf("Enter the radius and height of the cylinder: ");
+    scanf("%d %d", &d, &h1);
+    printf("The area of the cylinder = %f\n", CYLINDER(d, h1));
+
+    return 0;
 }
